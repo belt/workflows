835c3cff9482d335cdf3d3af8fe598d2c7655b8b
Paul Belt <paul.belt+git@gmail.com>
2017-02-10 13:18:39 -0500

    db/create_dev_user.sql for development and test

--- db/create_dev_user.sql
+++ db/create_dev_user.sql
@@ -1,6 +1,7 @@
-CREATE USER 'workflows-dev'@localhost IDENTIFIED BY 'changeme';
-GRANT ALL ON *.* to 'workflows-dev'@localhost IDENTIFIED BY 'changeme';
+CREATE USER 'workflows-development'@localhost IDENTIFIED BY 'ph1uff';
+GRANT ALL ON *.* to 'workflows-development'@localhost IDENTIFIED BY 'ph1uff';
+CREATE USER 'workflows-test'@localhost IDENTIFIED BY 'ph1uff';
+GRANT ALL ON *.* to 'workflows-test'@localhost IDENTIFIED BY 'ph1uff';
 FLUSH PRIVILEGES;
 CREATE DATABASE workflows_development;
 CREATE DATABASE workflows_test;
-CREATE DATABASE workflows_ci;
